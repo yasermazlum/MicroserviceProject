2fe8a6021d569fec6a2f5355c77948acd3d9f21d
Yaser <yasermazlum@gmail.com>
2023-06-03 21:53:06 +0300

    first

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -0,0 +1,11 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscription<T, TH>(IntegrationEvent @event) where T: IntegrationEvent where TH: IIntegrationEventHandler<T>;
+        void UnSubscription<T, TH>(IntegrationEvent @event) where T: IntegrationEvent where TH: IIntegrationEventHandler<T>;
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -0,0 +1,21 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBusSubscriptionManager
+    {
+        bool IsEmpty { get; }
+
+        event EventHandler<string> OnEventRemoved;
+
+        void AddSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+        void RemoveSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+        bool HashSubscriptionForEvent<T>() where T : IntegrationEvent;
+        bool HashSubscriptionForEvent(string eventName);
+        Type GetEventTypeByName(string eventName);
+        void Clear();
+        IEnumerable<SubscriptionInfo> GetHandlerForEvent<T>() where T : IntegrationEvent;
+        IEnumerable<SubscriptionInfo> GetHandlerForEvent(string eventName);
+        string GetEventKey<T>();
+    }
+}
